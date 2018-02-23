525992f9a41141ed7a759c73968ee822808737aa
Addison Crump <addisoncrump@tamu.edu>
2018-02-22 21:19:27 -0600

    Add copyright info

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# BanBot
+# BanBot ![](https://www.gnu.org/graphics/gplv3-88x31.png)
+
 A discord bot and an HTTP server which manages the banlist.
--- src/main/java/co/templex/banbot/discord/Bot.java
+++ src/main/java/co/templex/banbot/discord/Bot.java
@@ -1,3 +1,21 @@
+/*
+ * BanBot: A Discord bot and an HTTP server that manages the Templex banlist.
+ * Copyright (C) 2018  vtcakavsmoace
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package co.templex.banbot.discord;
 
 import com.google.common.util.concurrent.FutureCallback;
@@ -80,7 +98,7 @@ public class Bot {
                         if (latestLog.exists()) {
                             reader = new BufferedReader(new FileReader(latestLog));
                             //noinspection StatementWithEmptyBody
-                            while (reader.readLine() != null);
+                            while (reader.readLine() != null) ;
                         }
                         path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
                         WatchKey wk;
